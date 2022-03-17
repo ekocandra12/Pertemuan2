ae3a43436b8a94cd1082d21f765f14ec8bd7602f
Eko Candra <yteko12@gmail.com>
2022-03-17 15:39:42 +0700

    contoh penerapan return

---
+++ src/main/kotlin/t_return.kt
@@ -0,0 +1,21 @@
+fun main() {
+    returnLoop()
+}
+fun returnLoop(){
+    var x:Int
+    println("Example of return in For-Loop")
+    for(x in 1..10) {
+        if(x < 5) {
+            print("${x - 1} ")
+        }else {
+            return
+        }
+        print("$x ")
+    }
+    print("Tidak akan pernah dieksekusi")
+
+    /**
+     * Keyword return berguna untuk keluar dari fungsi dan mengembalikan suatu nilai kepada
+    pemanggil fungsi.
+     */
+}
