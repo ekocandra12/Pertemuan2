aabcd537e253debb312ae238189fa4ffb65236a8
Eko Candra <yteko12@gmail.com>
2022-03-23 19:53:22 +0700

    contoh kode penerapan safe call

---
+++ src/main/kotlin/u_pemanggilan_yang_aman.kt
@@ -0,0 +1,12 @@
+fun main() {
+//Safe calling
+//?.let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+//The ?. chaining
+//student?.teacher?.supervisor?.name = "Seymour Skinner"
+
+    /**
+     * berikut adalah contoh penerapan pemanggilan yang aman atau yang sering disebut dengan safe call
+     */
+}
