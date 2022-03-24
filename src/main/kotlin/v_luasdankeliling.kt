891aaaaaf643d3b594c8e832a31d8ea15bf67eb3
Eko Candra <yteko12@gmail.com>
2022-03-24 09:25:56 +0700

    contoh kode penerapan fungsi math menghiting luas dan keliling lingkaran

---
+++ src/main/kotlin/v_luasdankeliling.kt
@@ -0,0 +1,15 @@
+import kotlin.math.PI
+fun main() {
+//Advanced exercise
+    print("Enter the circle's radius (cm): ")
+    val radius = readLine()!!.toDouble()
+    var circumference = 2 * PI * radius
+    var area = PI * radius * radius
+    println("The circle's circumference based on the given radius is: " + circumference + " cm")
+    println("Area of the circle is: " + area + " cm^2")
+
+    /**
+     * berikut adalah penerapan fungsi math untuk menghitung luas dan keliling lingkaran di pemprograman kotlin
+     */
+
+}
