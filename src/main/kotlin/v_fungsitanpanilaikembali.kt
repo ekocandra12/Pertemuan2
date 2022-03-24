1de80aa943cec39237a2ca815abde94bae610c3c
Eko Candra <yteko12@gmail.com>
2022-03-24 09:28:27 +0700

    contoh kode penerapan fungsi tanpa nilai kembali

---
+++ src/main/kotlin/v_fungsitanpanilaikembali.kt
@@ -0,0 +1,14 @@
+fun main() {
+    displayMessage("Rekayasa Perangkat Lunak", 10);
+}
+fun displayMessage(msg: String, count: Int) {
+    var counter = 1
+    while(counter <= count ) {
+        println("$msg $counter")
+        counter++
+    }
+
+    /**
+     * berikut adalah penerapan fugsi tanpa nilai kembali
+     */
+}
