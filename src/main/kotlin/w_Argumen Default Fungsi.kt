42992a7cf9824587ce8c94f632fbb70ed4374150
Eko Candra <yteko12@gmail.com>
2022-03-24 09:41:53 +0700

    contoh kode penerapan argumen default fungsi

---
+++ src/main/kotlin/w_Argumen Default Fungsi.kt
@@ -0,0 +1,19 @@
+fun main() {
+//panggil tanpa argumen
+    connectToDb()
+    println()
+//panggil dengan 2 argumen
+    connectToDb("sqlserver", "depandi")
+}
+fun connectToDb(hostname: String = "localhost",
+                username: String = "mysql",
+                password:String = "secret") {
+    println("hostname : $hostname")
+    println("username : $username")
+    println("password : $password")
+
+    /**
+     * Parameter fungsi bisa memiliki nilai default yang membolehkan pemanggil fungsi untuk
+    menghilangkan beberapa argumen ketika dipanggil.
+     */
+}
